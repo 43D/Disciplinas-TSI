f0c25e14b3b386d46148f314e39de7dfc513a0e9
Allan Fel <allan.goioere@gmail.com>
2022-09-22 22:21:50 -0300

    Main AppView + Tester

---
+++ Padrões de Projeto/18-09-2022-solid/AppSequenceGenerators/src/test/app/view/ApplicationViewTest.java
@@ -0,0 +1,12 @@
+package test.app.view;
+
+import app.view.ApplicationView;
+
+public class ApplicationViewTest {
+
+	public static void main(String[] args) {
+		ApplicationView appView = ApplicationView.create();
+		appView.setVisible(true);
+	}
+
+}

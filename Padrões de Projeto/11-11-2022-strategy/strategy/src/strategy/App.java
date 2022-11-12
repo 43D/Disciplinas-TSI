4439fa980d317e00eadbf487fab162cbe5736683
Allan Fel <allan.goioere@gmail.com>
2022-11-12 20:54:49 -0300

    Up new zip

--- Padrões de Projeto/11-11-2022-strategy/strategy/src/strategy/App.java
+++ Padrões de Projeto/11-11-2022-strategy/strategy/src/strategy/App.java
@@ -2,9 +2,7 @@ package strategy;
 
 import java.time.Year;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class App {
 	private final Stock<Car> carStock;

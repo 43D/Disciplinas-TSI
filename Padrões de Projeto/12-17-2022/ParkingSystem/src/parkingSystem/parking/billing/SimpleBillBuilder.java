2543b2d5ee9b08acb8ff637a501744f0d6be0588
Allan <allan.2001@alunos.utfpr.edu.br>
2022-11-17 22:35:33 -0300

    New Project

---
+++ Padrões de Projeto/12-17-2022/ParkingSystem/src/parkingSystem/parking/billing/SimpleBillBuilder.java
@@ -0,0 +1,43 @@
+package parkingSystem.parking.billing;
+
+import java.time.LocalDateTime;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+public class SimpleBillBuilder {
+
+	private LocalDateTime parkingEntry;
+	private LocalDateTime parkingExit;
+	private List<Entry<String, Float>> descriptionList = new ArrayList<>();
+
+	public SimpleBillBuilder parkingEntry(LocalDateTime parkingEntry) {
+		this.parkingEntry = parkingEntry;
+		return this;
+	}
+
+	public SimpleBillBuilder parkingExit(LocalDateTime parkingExit) {
+		this.parkingExit = parkingExit;
+		return this;
+	}
+
+	public SimpleBillBuilder appendDescriptio(String description, float value) {
+		descriptionList.add(new SimpleEntry<String, Float>(description, value));
+		return this;
+	}
+
+	public SimpleBill build() {
+		SimpleBill bill = new SimpleBill(parkingEntry, parkingExit, descriptionList);
+		
+		this.reset();
+		
+		return bill;
+	}
+	
+	private void reset() {
+		parkingEntry = null;
+		parkingExit = null;
+		descriptionList = new ArrayList<>();
+	}
+}
